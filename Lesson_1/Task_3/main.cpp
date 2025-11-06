b296989cb464c299e186e08cc731b6a56499a127
evgeniy-gith <jenyagricyk@gmail.com>
2025-11-06 19:31:25 +0200

    ispravil oshibku

--- Lesson_1/Task_3/main.cpp
+++ Lesson_1/Task_3/main.cpp
@@ -1,51 +1,21 @@
-#include <iostream>;
+#include <iostream>
 using namespace std;
 int main() {
-	float arr[10]{ 0 };
-	int nomerscheta;
-	int deystvie;
-	float snyatie;
-	float popolnenie;
-	cout << "Vvedite nomer sceta (1-10): ";
-	cin >> nomerscheta;
-	cout << "Vash balans: " << arr[nomerscheta] << endl;
-	cout << "Viberete sleduyushchee deystvie: " << endl << "'1' sniat' den'gi" << endl
-		<< "'2' popolnit' balans" << endl << "'3' Vivesty balans vseh schetov" << endl
-		<< "'4' zavershit' rabotu" << endl;
-	cin >> deystvie;
-	switch (deystvie) {
-	case 1: {
-		cout << "Vvedite sumu dlya snyatiya: ";
-		cin >> snyatie;
-		if (snyatie > arr[nomerscheta]) {
-			cout << "Ne dostatochno sredstv" << endl;
-		}
-		else {
-			cout << "Uspeshno, vash tekushiy balans: " << arr[nomerscheta] - snyatie << endl;
-		}
-		break;
-	case 2: {
-		cout << "Vvedite summu dlya popolneniya:";
-		cin >> popolnenie;
-		cout << "Uspeshno, vash tekushiy balans: " << arr[nomerscheta] + snyatie << endl;
-		break;
-	}
-	case 3: {
-		for (int vsescheta = 0; vsescheta < 10; vsescheta++) {
-			cout << "Nomer sheta: " << vsescheta << "Balans: " << arr[vsescheta] << endl;
-		}
-	}
-	case 4: {
-		cout << "Rabota zavershena. Horesego dnya!" << endl;
-		break;
-	}
-
-	default: {
-		cout << "Neizvestnoe deystvie" << endl;
-		break;
-	}
-	}
-	}
-
+    int width, height, size;
+    cout << "Vvedite razmer figur: ";
+    cin >> width >> height >> size;
+    for (int numRow = 0; numRow < height; numRow++) {
+        for (int numCollon = 0; numCollon < width; numCollon++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    for (int numRow = 1; numRow <= size; numRow++) {
+        for (int numCollon = 0; numCollon < numRow; numCollon++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
 
